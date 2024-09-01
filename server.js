27abf0031e73d16f8189406082359261a333ff01
Shreyash Karandikar <shreyashkarandikar@Shreyashs-Laptop.local>
2024-09-01 17:09:29 -0400

    created simple node server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-// node.js hellow world program
-console.log("Hello World");
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('How are you?\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
